fd8fcf856fafa4d78f3c1213b43b5e2e1eb2f3c9
Lama AlMayouf <lamayf@hotmail.com>
2022-06-17 18:01:45 +0300

    add springdoc-openapi-ui

--- vehicles-api/src/main/java/com/udacity/vehicles/cofig/BeanConfig.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/cofig/BeanConfig.java
@@ -4,17 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class BeanConfig {
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     @Bean
     @LoadBalanced
